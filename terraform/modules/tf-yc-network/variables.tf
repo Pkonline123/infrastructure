9ba989e76e85501aa8f2bda688be0518f956ada8
Andrey <zachitaylov-l2@yandex.ru>
2023-02-21 12:51:09 +0300

    pravka6

--- terraform/modules/tf-yc-network/variables.tf
+++ terraform/modules/tf-yc-network/variables.tf
@@ -1,11 +1,11 @@
 variable "network_zones" {
-  default     = ["ru-central1-a"]
+  default     = ["ru-central1-a", "ru-central1-b", "ru-central1-c"]
   type        = set(string)
   description = "Instance availability zone"
-  validation {
-    condition     = contains(toset(["ru-central1-a", "ru-central1-b", "ru-central1-c"]), var.network_zones)
-    error_message = "Select availability zone from the list: ru-central1-a, ru-central1-b, ru-central1-c."
-  }
+  # validation {
+  #   condition     = contains(toset(["ru-central1-a", "ru-central1-b", "ru-central1-c"]), var.network_zones)
+  #   error_message = "Select availability zone from the list: ru-central1-a, ru-central1-b, ru-central1-c."
+  # }
   sensitive = true
   nullable  = false
 }
