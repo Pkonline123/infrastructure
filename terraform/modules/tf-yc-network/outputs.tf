41911102de3543257d401663e28a074f23ce34ef
Andrey <zachitaylov-l2@yandex.ru>
2023-02-21 12:36:01 +0300

    pravca4

--- terraform/modules/tf-yc-network/outputs.tf
+++ terraform/modules/tf-yc-network/outputs.tf
@@ -1,9 +1,9 @@
 output "yandex_vpc_network" {
   description = "vpc_network"
-  value = data.vpc_network.default.name
+  value = data.yandex_vpc_network.default.name
 }
 
 output "yandex_vpc_subnet" {
   description = "Yandex.Cloud Subnets map"
-  value       = data.vpc_subnet.default
+  value       = data.yandex_vpc_subnet.default
 }
--- terraform/modules/tf-yc-network/variables.tf
+++ terraform/modules/tf-yc-network/variables.tf
@@ -1,6 +1,6 @@
 variable "network_zones" {
   default     = "ru-central1-a"
-  type        = string
+  type        = set(string)
   description = "Instance availability zone"
   validation {
     condition     = contains(toset(["ru-central1-a", "ru-central1-b", "ru-central1-c"]), var.network_zones)
