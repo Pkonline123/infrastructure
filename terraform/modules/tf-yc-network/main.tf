14b40a01f6f5ccb4aab806979c00f7ee28e9df65
Andrey <zachitaylov-l2@yandex.ru>
2023-02-21 12:35:49 +0300

    pravca4

--- terraform/modules/tf-yc-network/main.tf
+++ terraform/modules/tf-yc-network/main.tf
@@ -5,5 +5,5 @@ data "yandex_vpc_network" "default" {
 data "yandex_vpc_subnet" "default" {
   for_each = toset(var.network_zones)
 
-  name = "${data.vpc_network.default.name}-${each.key}"
+  name = "${data.yandex_vpc_network.default.name}-${each.key}"
 }
