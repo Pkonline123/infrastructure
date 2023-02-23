6daa465dc3d412b46a68bfb800d0ca8b09b8ccc5
Andrey <zachitaylov-l2@yandex.ru>
2023-02-23 13:12:44 +0300

    Эксперементы

--- terraform/modules/tf-yc-network/main.tf
+++ terraform/modules/tf-yc-network/main.tf
@@ -3,7 +3,10 @@ data "yandex_vpc_network" "default" {
 }
 
 data "yandex_vpc_subnet" "default" {
+  #Старая реализация
   for_each = toset(var.network_zones)
-
   name = "${data.yandex_vpc_network.default.name}-${each.key}"
+  #Новая
+  # for_each = toset(data.yandex_vpc_network.default.subnets_ids)
+  # subnet_id = "${each.value}"
 }
--- terraform/modules/tf-yc-network/variables.tf
+++ terraform/modules/tf-yc-network/variables.tf
@@ -16,3 +16,14 @@ variable "zone" {
   sensitive = false
   nullable  = false
 }
+# variable "network_zones" {
+#   default     = "ru-central1-a"
+#   type        = string
+#   description = "Instance availability zone"
+#   validation {
+#     condition     = contains(toset(["ru-central1-a", "ru-central1-b", "ru-central1-c"]), var.network_zones)
+#     error_message = "Select availability zone from the list: ru-central1-a, ru-central1-b, ru-central1-c."
+#   }
+#   sensitive = false
+#   nullable  = false
+# }
